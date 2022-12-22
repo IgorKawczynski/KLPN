1350ff5e299d5255445749af38630f1632094b67
IgorKawczynski <kluseczkibabuni94@gmail.com>
2022-12-22 08:56:35 +0100

    Initalizing the project...

---
+++ src/main/java/com/io/klpn/KlpnApplication.java
@@ -0,0 +1,13 @@
+package com.io.klpn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlpnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlpnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/klpn/KlpnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.klpn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlpnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
