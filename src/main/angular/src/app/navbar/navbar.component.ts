d9f430f271feac25a2389355def41d5ee4b54ca8
KrystianKierklo <krystian.kierklo3@gmail.com>
2023-01-02 17:28:53 +0100

    navbar, footer, home

---
+++ src/main/angular/src/app/navbar/navbar.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  items: MenuItem[]=[];
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+
+    this.items = [
+      {label: 'Rezerwacja',
+        items:[
+          {
+            label:'Rezerwuj boisko',
+            icon:'pi pi-fw pi-calendar'
+          },
+          {
+            label:'Zarządzaj rezerwacją',
+            icon:'pi pi-fw pi-cog'
+          },
+          {
+            label:'Historia rezerwacji',
+            icon:'pi pi-fw pi-clock'
+          }
+            ]
+      },
+      {label: 'Drużyna',
+        items:[
+          {
+            label: 'Zarejestruj drużynę',
+            icon: 'pi pw-fw pi-pencil'
+          },
+          {
+            label: 'Zarządzaj drużyną',
+            icon: 'pi pw-fw pi-user-edit'
+          },
+          {
+            label: 'Transfer',
+            icon: 'pi pw-fw pi-arrow-right-arrow-left'
+          },
+        ]},
+      {label: 'Tabela'},
+      {label: 'Harmonogram'},
+      {label: 'Kontakt'}
+      ];
+
+  }
+
+}
