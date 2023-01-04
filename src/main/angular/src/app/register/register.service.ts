3dcb925ebee62c5418cb36ed8d084b7425fb1061
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-01-04 19:50:38 +0100

    Added Registration Service...

--- src/main/angular/src/app/register/register.service.ts
+++ src/main/angular/src/app/register/register.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import {UserRequestDTO} from "../user/user-request";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public register(user: UserRequestDTO): Observable<Object> {
+    return this.http.post<UserRequestDTO>(`${this.apiServerUrl}/api/user/register`, user);
+  }
+
 }
