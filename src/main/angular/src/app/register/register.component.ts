20231279e84f140fec2325edf4e20807ec1731ea
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-01-04 21:32:08 +0100

    Modified register component with register's service method implementation, modified its style, its inputs, added pToast to app...

--- src/main/angular/src/app/register/register.component.ts
+++ src/main/angular/src/app/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {UserRequestDTO} from "../user/user-request";
+import {ErrorsListDTO} from "../basic/error-list/error-list";
+import {MessageService, PrimeNGConfig} from "primeng/api";
+import {RegisterService} from "./register.service";
+import {Router} from "@angular/router";
+import {SessionService} from "../session/session.service";
 
 @Component({
   selector: 'app-register',
@@ -7,11 +13,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  models: any = {};
+  userRequestDto: UserRequestDTO = new UserRequestDTO();
+  errorsListDto: ErrorsListDTO = new ErrorsListDTO();
 
-  constructor() { }
+  constructor(
+    private registerService: RegisterService,
+    private primengConfig: PrimeNGConfig,
+    private router: Router,
+    private messageService: MessageService,
+    private sessionService: SessionService
+  ){
+  }
 
   ngOnInit(): void {
+  }
+
+  btnRegister(): void {
+    console.log(this.userRequestDto);
+    this.registerUser();
+  }
+
+  btnRoute(url: string) {
+    this.router.navigateByUrl(url);
+  }
+
+  public registerUser() {
+    this.registerService
+      .register(this.userRequestDto)
+      .subscribe( (response: any) => {
+        this.errorsListDto = response;
+        console.log(this.errorsListDto);
+        if(!this.errorsListDto.listOfErrorsEmpty) {
+          this.errorsListDto.errors.forEach((error) =>
+            this.messageService.add({life:3000, severity:'error', summary:'Register', detail:error})
+          );
+        }
+        else{
+          this.messageService.add({life: 3000, severity:'success', summary:'Register', detail:'You have successfully signed up!'});
+          this.messageService.add({life: 5500, severity:'info', summary:'Register', detail:'Now you can log in with your credentials!'});
+          this.router.navigateByUrl('/login');
+        }
+      });
   }
 
 }
