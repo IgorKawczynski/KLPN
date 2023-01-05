f91be7d8d17ceeed08d693470b465bc426adced0
Adam <trentowski.adam@gmail.com>
2023-01-05 10:39:43 +0100

    Added match statistic controller

--- src/main/java/com/io/klpn/match_statistic/MatchStatisticController.java
+++ src/main/java/com/io/klpn/match_statistic/MatchStatisticController.java
@@ -1,14 +1,42 @@
 package com.io.klpn.match_statistic;
 
+import com.io.klpn.basic.ErrorsListDTO;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/match-statistics")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class MatchStatisticController {
+
+    final MatchStatisticService matchStatisticService;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public MatchStatistic getMatchStatisticById(@PathVariable Long id){
+        return matchStatisticService.getMatchStatisticById(id);
+    }
+
+    @GetMapping("/list")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<MatchStatistic> getAllMatchStatistics(@RequestParam(value = "page", required = false, defaultValue = "0") Integer page) {
+        return matchStatisticService.getAllMatchStatistics(page);
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ErrorsListDTO createMatchStatistic(@RequestBody MatchStatistic matchStatistic) {
+        return matchStatisticService.createMatchStatistic(matchStatistic);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ErrorsListDTO deleteMatchStatisticById(@PathVariable Long id){
+        return matchStatisticService.deleteMatchStatisticById(id);
+    }
 }
