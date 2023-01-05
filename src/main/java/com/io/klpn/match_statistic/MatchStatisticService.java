3defdd0473ce4fc0ff6c18420d5cb7487fc1cfe6
Adam <trentowski.adam@gmail.com>
2023-01-05 10:39:30 +0100

    Added match statistic service

--- src/main/java/com/io/klpn/match_statistic/MatchStatisticService.java
+++ src/main/java/com/io/klpn/match_statistic/MatchStatisticService.java
@@ -1,12 +1,57 @@
 package com.io.klpn.match_statistic;
 
+import com.io.klpn.basic.ErrorsListDTO;
+import com.io.klpn.basic.exceptions.IntegerValidatorException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class MatchStatisticService {
+
+    final MatchStatisticRepository matchStatisticRepository;
+
+    final MatchStatisticValidator matchStatisticValidator;
+    public ErrorsListDTO createMatchStatistic(MatchStatistic matchStatisticToCreate){
+        var errorsList = new ErrorsListDTO();
+
+        try {
+            var matchStatistic = matchStatisticValidator.createMatchStatistic(matchStatisticToCreate);
+            matchStatisticRepository.save(matchStatistic);
+        }
+        catch (IntegerValidatorException exception) {
+            errorsList.addError(exception.getMessage());
+        }
+        return errorsList;
+    }
+
+    public MatchStatistic getMatchStatisticById(Long id){
+        return matchStatisticRepository
+                .findById(id)
+                .orElseThrow(() -> new NoSuchElementException(String.format("Match statistic with id: %d does not exist. ", id)));
+    }
+
+    public Page<MatchStatistic> getAllMatchStatistics(Integer page){
+        Pageable paginated = PageRequest.of(page, 20);
+        return matchStatisticRepository.findAll(paginated);
+    }
+
+    public ErrorsListDTO deleteMatchStatisticById(Long id) {
+        var errorsListDTO = new ErrorsListDTO();
+        try {
+            matchStatisticValidator.deleteMatchStatisticById(id);
+        }
+        catch (NoSuchElementException exception) {
+            errorsListDTO.addError(exception.getMessage());
+        }
+        return errorsListDTO;
+    }
 }
