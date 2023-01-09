ed3ac219dff522352d08348857cd4e5753729f25
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-01-09 23:16:11 +0100

    Modified Security Configuration...

--- src/main/java/com/io/klpn/security/AuthenticationConfiguration.java
+++ src/main/java/com/io/klpn/security/AuthenticationConfiguration.java
@@ -2,30 +2,75 @@ package com.io.klpn.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
-public class AuthenticationConfiguration {
+public class AuthenticationConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(
-                        (authz) -> authz
-                        .anyRequest().permitAll()
-                )
-                .httpBasic(Customizer.withDefaults());
+    private final CurrentUserService currentUserService;
+    private final SessionFilter sessionFilter;
+    private final PasswordEncoder passwordEncoder;
 
-        http
+    @Lazy
+    public AuthenticationConfiguration(CurrentUserService currentUserService, SessionFilter sessionFilter, PasswordEncoder passwordEncoder) {
+        this.currentUserService = currentUserService;
+        this.sessionFilter = sessionFilter;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(currentUserService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http = http
                 .cors()
                 .and()
                 .csrf()
                 .disable();
 
-        return http.build();
+        http = http
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, exception) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                                exception.getMessage()))
+                .and();
+
+        http
+                .authorizeRequests()
+                .antMatchers("/api/user/login", "/api/**", "/", "/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        http
+                .addFilterBefore(sessionFilter, UsernamePasswordAuthenticationFilter.class);
+
     }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
+
