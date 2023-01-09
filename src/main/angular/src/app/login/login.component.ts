119cbac55b761db935e8431c600485ef4dff5f22
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-01-09 23:19:55 +0100

    Added login's service front-end side...

--- src/main/angular/src/app/login/login.component.ts
+++ src/main/angular/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import {MessageService, PrimeNGConfig} from "primeng/api";
+import { ErrorsListDTO } from '../basic/error-list/error-list'
 
 @Component({
   selector: 'app-login',
@@ -6,14 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  models: any = {};
 
-  constructor() { }
+  model: any = {};
+  sessionId: any = "";
+  errorsListDTO: ErrorsListDTO = new ErrorsListDTO();
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private primengConfig: PrimeNGConfig,
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit(): void { }
+
+  btnLogin() {
+    this.login();
+  }
+
+  login() {
+    let url = '/api/user/login';
+    this.http.post<any>(url, {
+      email: this.model.email,
+      password: this.model.password
+    }).subscribe(
+      res => {
+        console.log(res);
+        if (res.sessionId != null) {
+          this.sessionId = res.sessionId;
+          sessionStorage.setItem('token', this.sessionId);
+          this.router.navigate(['localhost:4200/']);
+          this.messageService.add({life:3000, severity:'success', summary:'Login', detail:" Udało ci się zalogować !"})
+          localStorage.setItem('email', this.model.email)
+        }
+        else {
+          res.errorsListDTO.errors.forEach((error: any) =>
+            this.messageService.add({life:10000, severity:'error', summary:'Login', detail: error})
+          );
+        }
+      });
 
   }
-  btnLogin() {
+  history() {
+
   }
 
 
