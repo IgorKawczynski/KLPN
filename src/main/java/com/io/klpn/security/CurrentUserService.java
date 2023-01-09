1cdb6691e4eca53547c5983b83d9a0df3c5dff23
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-01-09 23:16:45 +0100

    Added User's Security service + User Details implementation

---
+++ src/main/java/com/io/klpn/security/CurrentUserService.java
@@ -0,0 +1,30 @@
+package com.io.klpn.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.io.klpn.user.UserRepository;
+import com.io.klpn.user.User;
+
+@Service
+public class CurrentUserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CurrentUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override //Nazwa metody `LoadByUsername` ze wzgledu na to, ze to metoda pobierana z interfejsu UserDetailsService
+    public CurrentUser loadUserByUsername(String email) throws UsernameNotFoundException {
+        final User user = userRepository.findByEmail(email);
+        if (user != null) {
+            final CurrentUser currentUser = new CurrentUser();
+            currentUser.setEmail(user.getEmail());
+            currentUser.setPassword(user.getPassword());
+            return currentUser;
+        }
+        else
+            throw new UsernameNotFoundException("Failed to find user with email: " + email);
+    }
+}
