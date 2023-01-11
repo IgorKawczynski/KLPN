c0de222c0bcdefb2508927a72ee3ded7ab25b4e5
KrystianKierklo <krystian.kierklo3@gmail.com>
2023-01-11 01:05:41 +0100

    schedule page

--- src/main/angular/src/app/schedule/schedule.component.ts
+++ src/main/angular/src/app/schedule/schedule.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {TreeNode} from 'primeng/api';
+import * as Http from "http";
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
   styleUrls: ['./schedule.component.scss']
 })
+
+
 export class ScheduleComponent implements OnInit {
 
+  files: TreeNode[] = [];
+  //data = formatDate(Date.now(),'dd-MM-yyyy','en-US');
+  selectedNode: TreeNode | undefined;
+  cols: any[] = [];
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.files = [];
+    for(let i = 1; i <= 7; i++) {
+      let node = {
+        data:{
+          date: 'Data' + i,
+        },
+        children: [
+          {
+            data: {
+              team1: 'Kortowskie szczury',
+              team2: 'Dzikie ify',
+              hour: '17.00',
+              pitch_number: 3,
+            }
+          },
+          {
+            data: {
+              team1: 'Kortowskie szczury',
+              team2: 'Dzikie ify',
+              hour: '17.00',
+              pitch_number: 3,
+            }
+          },
+          {
+            data: {
+              team1: 'Kortowskie szczury',
+              team2: 'Dzikie ify',
+              hour: '17.00',
+              pitch_number: 3,
+            }
+          },
+          {
+            data: {
+              team1: 'Kortowskie szczury',
+              team2: 'Dzikie ify',
+              hour: '17.00',
+              pitch_number: 3,
+            }
+          },
+
+          {
+            data: {
+              team1: 'Kozaki z Olsztyna',
+              team2: 'Zwierzęta',
+              hour: '17.00',
+              pitch_number: 1,
+            }
+          },
+        ]
+      };
+
+      this.files.push(node);
+    }
+
+    this.cols = [
+      { field: 'date', header: 'Data' },
+      { field: 'team1', header: 'Pierwsze drużyna' },
+      { field: 'team2', header: 'Druga druzyna' },
+      { field: 'hour', header: 'Godzina spotkania' },
+      { field: 'pitch_number', header: 'Numer boiska' }
+    ];
   }
 
 }
