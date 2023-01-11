482a68d74ecfa8f2a81947c790c2905a21b502d6
GKrych <hazzidy125@gmail.com>
2023-01-11 23:56:42 +0100

    Added team component at frontend, and needed methods to perform team registration logic.

---
+++ src/main/angular/src/app/student/student-player-dto.ts
@@ -0,0 +1,7 @@
+import { Position } from "./position-enum";
+
+export class StudentPlayerDTO{
+  indexNumber!: number;
+  position!: Position;
+  isReferee!: boolean;
+}
--- src/main/angular/src/app/student/student-player.ts
+++ src/main/angular/src/app/student/student-player.ts
@@ -4,4 +4,11 @@ export class StudentPlayer{
   indexNumber!: number;
   position!: Position;
   isReferee!: boolean;
+
+  constructor(indexNumber: number, position: Position, isReferee: boolean) {
+    this.indexNumber = indexNumber;
+    this.position = position;
+    this.isReferee = isReferee;
+  }
+  
 }
