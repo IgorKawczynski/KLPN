fbb04cdc25132dd0a17306af931bfdaf3e05ff34
Adam <trentowski.adam@gmail.com>
2023-01-12 23:28:40 +0100

    Added deleting and editing reservation and changed view of reservation-edit

--- src/main/angular/src/app/reservation-list/reservation-list.component.ts
+++ src/main/angular/src/app/reservation-list/reservation-list.component.ts
@@ -4,6 +4,7 @@ import { ErrorsListDTO } from '../basic/error-list/error-list';
 import { ReservationResponseDTO } from './reservation-response';
 import { ReservationListService } from './reservation-list.service';
 import { LoginService } from '../login/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reservation-list',
@@ -20,7 +21,8 @@ export class ReservationListComponent implements OnInit {
   constructor(
     private reservationListService: ReservationListService,
     private messageService: MessageService,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -37,12 +39,26 @@ export class ReservationListComponent implements OnInit {
 
   public btnDelete(rowId: number){
     this.deleteReservationById(rowId);
-    window.location.reload();
-    console.log(rowId);
+  }
+
+  public btnEdit(rowId: number){
+    this.router.navigateByUrl(`/reservation-edit/${rowId}`);
   }
 
   public deleteReservationById(rowId: number): void {
     this.reservationListService.deleteReservationById(rowId).subscribe((response: any) =>{
-    });
+      this.errorsListDto = response;
+      if(!this.errorsListDto.listOfErrorsEmpty) {
+        this.errorsListDto.errors.forEach((error) => {
+          this.messageService.add({life: 8000, severity:'error', summary:'Błąd', detail:error})
+        });
+      }
+      else{
+        this.messageService.add({life: 8000, severity:'success', summary:'Usunięto', detail:'Pomyślnie usunięto rezerwację!'});
+        setTimeout(location.reload.bind(location), 1500);
+      }
+    })
   }
+
+
 }
