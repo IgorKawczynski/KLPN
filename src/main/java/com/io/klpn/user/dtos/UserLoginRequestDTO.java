380c5c08313a3179240af1813701cce63b25a7a4
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-01-12 15:09:41 +0100

    Modifying user's login request DTO...

--- src/main/java/com/io/klpn/user/dtos/UserLoginRequestDTO.java
+++ src/main/java/com/io/klpn/user/dtos/UserLoginRequestDTO.java
@@ -2,7 +2,7 @@ package com.io.klpn.user.dtos;
 
 import lombok.Builder;
 
-public record UserLoginRequestDTO(Long id, String email, String password, Boolean isAdmin) {
+public record UserLoginRequestDTO(String email, String password, Boolean isAdmin) {
 
     @Builder
     public UserLoginRequestDTO {}
