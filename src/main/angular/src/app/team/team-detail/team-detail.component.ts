36b43d11440a578e11601b78c2df4601eefba159
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-01-13 22:07:57 +0100

    Team-detail component overhaul remake

--- src/main/angular/src/app/team/team-detail/team-detail.component.ts
+++ src/main/angular/src/app/team/team-detail/team-detail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {TeamDetailService} from "./team-detail.service";
-import {playerAndStatsDTOs} from "./playerAndStatsDTOs";
-import { LoginService } from '../../login/login.service';
+import {PlayerStatsDTO} from "./PlayerStatsDTO";
+import {LoginService} from '../../login/login.service';
+import {ActivatedRoute, Router} from "@angular/router";
 @Component({
   selector: 'app-team-detail',
   templateUrl: './team-detail.component.html',
@@ -9,11 +10,32 @@ import { LoginService } from '../../login/login.service';
 })
 export class TeamDetailComponent implements OnInit {
 
-  public playerstats: playerAndStatsDTOs[] = [];
-  constructor(private loginService: LoginService, private TeamDetailService: TeamDetailService) { }
+  public playerstats: PlayerStatsDTO[] = [];
+  teamName: string = "";
+  id: any;
+
+  constructor(
+    private loginService: LoginService,
+    private teamDetailService: TeamDetailService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.getDetail();
+    console.log(this.id);
+    console.log(this.teamName);
   }
 
-
+  public getDetail() {
+    this.teamDetailService
+      .getTeamDetail(this.id)
+      .subscribe( (response: any) => {
+        this.playerstats = response.playerAndStatsDTOs;
+        this.teamName = response.Name;
+        console.log(response);
+        console.log(this.playerstats);
+      });
+  }
 }
--- src/main/angular/src/app/team/team-detail/team-detail.service.ts
+++ src/main/angular/src/app/team/team-detail/team-detail.service.ts
@@ -2,9 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-
-import {playerAndStatsDTOs} from "./playerAndStatsDTOs";
 import {TeamDetailComponent} from "./team-detail.component";
+import {PlayerStatsDTO} from "./PlayerStatsDTO";
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +13,8 @@ export class TeamDetailService {
   private apiServerUrl = environment.apiBaseUrl;
   constructor(private http: HttpClient) { }
 
-  public getTeamDetail(teamId: number): Observable<playerAndStatsDTOs[]>{
-    return this.http.get<playerAndStatsDTOs[]>(`${this.apiServerUrl}/api/table"/details/1001`)
+  public getTeamDetail(teamId: number): Observable<PlayerStatsDTO[]>{
+    return this.http.get<PlayerStatsDTO[]>(`${this.apiServerUrl}/api/teams/details/${teamId}`);
   }
 
 
