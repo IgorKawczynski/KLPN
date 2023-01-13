933ad30ea361cb828c0aa0e590cc43bcdc23b71f
Adam <trentowski.adam@gmail.com>
2023-01-13 23:55:29 +0100

    Added match response dto

---
+++ src/main/java/com/io/klpn/match/dtos/MatchResponseDTO.java
@@ -0,0 +1,15 @@
+package com.io.klpn.match.dtos;
+
+import lombok.Builder;
+
+public record MatchResponseDTO(String firstTeamName,
+                               String secondTeamName,
+                               String date,
+                               Integer firstTeamGoals,
+                               Integer secondTeamGoals,
+                               String refereeFullName) {
+
+    @Builder
+    public MatchResponseDTO {}
+
+}
