d548c8c15f1fe29a7f3df64bc803741bb0bfd0d2
Adam <trentowski.adam@gmail.com>
2023-01-13 23:56:45 +0100

    Added queries to repositories to find matches

--- src/main/java/com/io/klpn/match/MatchRepository.java
+++ src/main/java/com/io/klpn/match/MatchRepository.java
@@ -1,16 +1,12 @@
 package com.io.klpn.match;
 
-import com.io.klpn.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
 
-    @Query("SELECT r from Reservation r INNER JOIN User u on r.user = u where u.isAdmin = true")
-    List<Reservation> getReservationsForAdmin();
+    Match findMatchByReservation_Id (Long id);
 
 }
--- src/main/java/com/io/klpn/reservation/ReservationRepository.java
+++ src/main/java/com/io/klpn/reservation/ReservationRepository.java
@@ -1,6 +1,8 @@
 package com.io.klpn.reservation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -13,5 +15,17 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     List<Reservation> findReservationsByUser_IdAndDateAfter(Long userId, LocalDateTime date);
 
     List<Reservation> findReservationsByUser_IdAndDateBefore(Long userId, LocalDateTime date);
+
+    @Query("SELECT r from Reservation r INNER JOIN User u on r.user = u where u.isAdmin = true")
+    List<Reservation> getReservationsForAdmin();
+
+    @Query("""
+                SELECT r FROM Reservation r
+                INNER JOIN User u ON r.user = u
+                WHERE u.isAdmin = true
+                AND r.date >= :startDate AND r.date < :endDate
+            """)
+    List<Reservation> getReservationsForAdminAndDate(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+
 
 }
