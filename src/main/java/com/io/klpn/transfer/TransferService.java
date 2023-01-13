e674f0543a86632f825b6f96688827fe5663ce5f
GKrych <hazzidy125@gmail.com>
2023-01-13 23:42:39 +0100

    Added rest needed for transfer

--- src/main/java/com/io/klpn/transfer/TransferService.java
+++ src/main/java/com/io/klpn/transfer/TransferService.java
@@ -1,6 +1,12 @@
 package com.io.klpn.transfer;
 
 import com.io.klpn.basic.ErrorsListDTO;
+import com.io.klpn.student.StudentRepository;
+import com.io.klpn.student.StudentService;
+import com.io.klpn.team.TeamRepository;
+import com.io.klpn.team.TeamService;
+import com.io.klpn.team.TeamValidator;
+import com.io.klpn.transfer.dto.SingleTransferDTO;
 import com.io.klpn.transfer.dto.TransferRequestDTO;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -19,13 +25,34 @@ public class TransferService {
 
     final TransferRepository transferRepository;
     final TransferValidator transferValidator;
+    final TeamRepository teamRepository;
+    final TeamValidator teamValidator;
+    final StudentService studentService;
+    final StudentRepository studentRepository;
 
     public void createHeadForHeadTransfer(TransferRequestDTO transferRequestDTO){
         transferRepository.save(transferValidator.createHeadForHeadTransfer(transferRequestDTO.firstId(), transferRequestDTO.secondId()));
     }
 
-    public void createSingleTransfer(TransferRequestDTO transferRequestDTO){
-        transferRepository.save(transferValidator.createSingleTransfer(transferRequestDTO.firstId(), transferRequestDTO.secondId()));
+    public ErrorsListDTO createSingleTransfer(SingleTransferDTO singleTransferDTO) {
+        var errorsList = new ErrorsListDTO();
+
+        try {
+            transferValidator.validateStudentBeforeTransfer(singleTransferDTO.newPlayerIndex());
+            var captain = studentService.getStudentById(singleTransferDTO.captainId());
+            var captainTeam = teamRepository.getReferenceById(captain.getTeam().getId());
+            teamValidator.teamSizeLessThanTwelve(captainTeam.getId());
+            var newPlayer = studentService.getStudentByIndexNumber(singleTransferDTO.newPlayerIndex());
+            var tranfser = transferValidator.createSingleTransfer(captainTeam.getId(), newPlayer.getId());
+            transferRepository.save(tranfser);
+            newPlayer.setTeam(captainTeam);
+            studentRepository.save(newPlayer);
+        }
+        catch (IllegalStateException exception) {
+            errorsList.addError(exception.getMessage());
+        }
+
+        return errorsList;
     }
 
     public Transfer getTransferById(Long id){
