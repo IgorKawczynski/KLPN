52767614e6d6c86d7cdeafb8e5c5a633b3ab0ed0
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-01-14 18:17:34 +0100

    Navbar updated (captain role checker)

--- src/main/angular/src/app/navbar/navbar.component.ts
+++ src/main/angular/src/app/navbar/navbar.component.ts
@@ -21,54 +21,101 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
     if(this.loginService.isLogged()) {
       if(this.loginService.isStudent()) {
-        this.items = [
-          {
-            label: 'Rezerwacja',
-            items: [
-              {
-                label: 'Rezerwuj boisko',
-                icon: 'pi pi-fw pi-calendar',
-                routerLink: "/reservation"
-              },
-              {
-                label:'Zarządzaj rezerwacją',
-                icon:'pi pi-fw pi-cog',
-                routerLink: "/reservation-list"
-              },
-              {
-                label: 'Historia rezerwacji',
-                icon: 'pi pi-fw pi-clock',
-                routerLink: "/reservation-history"
-              }
-            ]
-          },
-          {
-            label: 'Drużyna',
-            items: [
-              {
-                label: 'Zarejestruj drużynę',
-                icon: 'pi pw-fw pi-pencil',
-                routerLink: "/team"
-              },
-              {
-                label: 'Zarządzaj drużyną',
-                icon: 'pi pw-fw pi-user-edit',
-                routerLink: "TU ID DRUZYNY AKTUALNEJ DLA UZYTKOWNIKA< JESLI JEJ NIE MA TO PTOAST"
-              },
-              {
-                label: 'Transfer',
-                icon: 'pi pw-fw pi-arrow-right-arrow-left',
-                routerLink: "/transfer"
-              },
-            ]
-          },
-          {label: 'Tabela', routerLink: "/table"},
-          {label: 'Harmonogram', routerLink: "/schedule"},
-          {label: 'Kontakt', routerLink: "/contact"}
-        ];
-        this.itemsRight =  [
-          {label: 'Witaj ' + this.loginService.getName() || undefined, routerLink: "/users/my-profile"}
-        ]
+        if (this.loginService.getRole() == 'CAPTAIN') {
+          this.items = [
+            {
+              label: 'Rezerwacja',
+              items: [
+                {
+                  label: 'Rezerwuj boisko',
+                  icon: 'pi pi-fw pi-calendar',
+                  routerLink: "/reservation"
+                },
+                {
+                  label: 'Zarządzaj rezerwacją',
+                  icon: 'pi pi-fw pi-cog',
+                  routerLink: "/reservation-list"
+                },
+                {
+                  label: 'Historia rezerwacji',
+                  icon: 'pi pi-fw pi-clock',
+                  routerLink: "/reservation-history"
+                }
+              ]
+            },
+            {
+              label: 'Drużyna',
+              items: [
+                {
+                  label: 'Zarejestruj drużynę',
+                  icon: 'pi pw-fw pi-pencil',
+                  routerLink: "/team"
+                },
+                {
+                  label: 'Zarządzaj drużyną',
+                  icon: 'pi pw-fw pi-user-edit',
+                  routerLink: "/student/my-team"
+                },
+                {
+                  label: 'Transfer',
+                  icon: 'pi pw-fw pi-arrow-right-arrow-left',
+                  routerLink: "/transfer"
+                },
+              ]
+            },
+            {label: 'Tabela', routerLink: "/table"},
+            {label: 'Harmonogram', routerLink: "/schedule"},
+            {label: 'Kontakt', routerLink: "/contact"}
+          ];
+          this.itemsRight = [
+            {label: 'Witaj ' + this.loginService.getName() || undefined, routerLink: "/users/my-profile"}
+          ]
+        }
+        else {
+          this.items = [
+            {
+              label: 'Rezerwacja',
+              items: [
+                {
+                  label: 'Rezerwuj boisko',
+                  icon: 'pi pi-fw pi-calendar',
+                  routerLink: "/reservation"
+                },
+                {
+                  label: 'Zarządzaj rezerwacją',
+                  icon: 'pi pi-fw pi-cog',
+                  routerLink: "/reservation-list"
+                },
+                {
+                  label: 'Historia rezerwacji',
+                  icon: 'pi pi-fw pi-clock',
+                  routerLink: "/reservation-history"
+                }
+              ]
+            },
+            {
+              label: 'Drużyna',
+              items: [
+                {
+                  label: 'Zarejestruj drużynę',
+                  icon: 'pi pw-fw pi-pencil',
+                  routerLink: "/team"
+                },
+                {
+                  label: 'Transfer',
+                  icon: 'pi pw-fw pi-arrow-right-arrow-left',
+                  routerLink: "/transfer"
+                },
+              ]
+            },
+            {label: 'Tabela', routerLink: "/table"},
+            {label: 'Harmonogram', routerLink: "/schedule"},
+            {label: 'Kontakt', routerLink: "/contact"}
+          ];
+          this.itemsRight = [
+            {label: 'Witaj ' + this.loginService.getName() || undefined, routerLink: "/users/my-profile"}
+          ]
+        }
       }
       else {
         this.items = [
@@ -99,11 +146,6 @@ export class NavbarComponent implements OnInit {
                 label: 'Zarejestruj drużynę',
                 icon: 'pi pw-fw pi-pencil',
                 routerLink: "/team"
-              },
-              {
-                label: 'Zarządzaj drużyną',
-                icon: 'pi pw-fw pi-user-edit',
-                routerLink: "/team-edit"
               },
               {
                 label: 'Transfer',
